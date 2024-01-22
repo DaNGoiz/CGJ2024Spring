fafa44fcf9425fb6eca27b59f8d8fc4a00317e42
Izzy <3332099165@qq.com>
2024-01-22 21:52:30 +0800

    Update Map

--- Assets/Scripts/Map/GenerateSingleRoom.cs
+++ Assets/Scripts/Map/GenerateSingleRoom.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+/// <summary>
+/// 生成单个房间，包括地板、墙壁、装饰物等
+/// </summary>
 public class GenerateSingleRoom : MonoBehaviour
 {
 
     [Header("Tiles")]
-    public Tile[] groundTiles;
-    public Tile[] decoTiles;
-    private Tilemap groundTilemap;
+    public Tile[] tiles;
+    private Tilemap tilemap;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Tilemap>().ClearAllTiles();
+        tilemap = GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
