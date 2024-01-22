b785813205eba46214909cb86f6f6e39e35b66a7
Izzy <3332099165@qq.com>
2024-01-22 16:06:32 +0800

    Create Map Tile

---
+++ Assets/Scripts/Map/GenerateSingleRoom.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateSingleRoom : MonoBehaviour
+{
+
+    [Header("Tiles")]
+    public Tile[] groundTiles;
+    public Tile[] decoTiles;
+    private Tilemap groundTilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
