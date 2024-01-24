497721b2f3224c5a82fd8f4ab1f2afa171e4b358
Tai2Cd <1303631444@qq.com>
2024-01-24 20:37:58 +0800

    UI操作
    生命值UI操作的加血扣血实现

--- Assets/Scripts/Players/P1Life.cs
+++ Assets/Scripts/Players/P1Life.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class P1Life : MonoBehaviour
 {
+    //SF
+    [SerializeField]
     private int life;
+    
+    //NoSF
     private bool unDead;
+    static public int lifeP1;
+
+    public void Damage(int value)
+    {
+        lifeP1 -= value;
+        if(lifeP1<0)
+        {
+
+            //dead
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         life = 10;
         unDead = true;
+        lifeP1 = life;
     }
 
     // Update is called once per frame
@@ -24,5 +40,7 @@ public class P1Life : MonoBehaviour
         {
             life = 10;
         }
+        lifeP1 = life;
     }
+
 }
