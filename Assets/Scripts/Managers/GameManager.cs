527f87c11fce9d46ecb63913f30cc0727f8ddaba
Tai2Cd <1303631444@qq.com>
2024-01-26 00:04:21 +0800

    GameManager加入角色相关的事件函数
    尝试先套用一下最开始那个模板，单例模式不是太熟悉（（（（（（（（（（（（

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -13,4 +13,20 @@ public class GameManager : Sington<GameManager>
     {
         EventCenter.Broadcast<bool>(EventCode.SwtichState, isChangeState);
     }
+    /// <summary>
+    /// 玩家处于忍受状态时的状态切换
+    /// </summary>
+    /// <param name="isChangeState">true为改变 false为未改变</param>
+    public void SwitchInTolerant(bool isChangeState)
+    {
+        EventCenter.Broadcast<bool>(EventCode.SwtichState, isChangeState);
+    }
+    /// <summary>
+    /// 当玩家从忍受转变为迎战的状态切换
+    /// </summary>
+    /// <param name="isChangeState">true为改变 false为未改变</param>
+    public void SwitchInTrigger(bool isChangeState)
+    {
+        EventCenter.Broadcast<bool>(EventCode.SwtichState, isChangeState);
+    }
 }
