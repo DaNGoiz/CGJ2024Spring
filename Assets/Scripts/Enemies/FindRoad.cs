1f7638234480fd19e82f0fce8ab7a5a5912b6b3e
Tai2Cd <1303631444@qq.com>
2024-01-28 11:45:43 +0800

    敌人寻路脚本完成

--- Assets/Scripts/Enemies/FindRoad.cs
+++ Assets/Scripts/Enemies/FindRoad.cs
@@ -7,15 +7,31 @@ public class FindRoad : MonoBehaviour
     [SerializeField]
     public Transform target;
     private NavMeshAgent agent;
+    
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        ChoosePlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.position);
+    }
+
+    //随机选择玩家进行追踪和射击
+    void ChoosePlayer()
+    {
+        int random = Random.Range(0, 99);
+        if (random < 50)
+        {
+            target = GameObject.Find("Player1").transform;
+        }
+        else
+        {
+            target = GameObject.Find("Player2").transform;
+        }
     }
 }
